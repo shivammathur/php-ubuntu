710343663a5bb164a8b7f47d0d8c3ce59506dea9
Shivam Mathur <shivam_jpr@hotmail.com>
2021-03-27 10:01:59 +0530

    Add apache
    Remove packages giving warnings

--- src/package.sh
+++ src/package.sh
@@ -6,6 +6,8 @@ mkdir -p /tmp/php
 for file in $(git log -p -n 1 --name-only | sed 's/^.*\(\s\).*$/\1/' | xargs -L1 echo); do
   sudo cp -r -p --parents "$file" /tmp/php || true
 done
+sudo cp /etc/apt/sources.list.d/ondrej* /tmp/php/etc/apt/sources.list.d
+sudo cp /etc/apt/trusted.gpg.d/ondrej* /tmp/php/etc/apt/trusted.gpg.d
 sudo rm -rf /tmp/php/var/lib/dpkg/alternatives/*
 . /etc/lsb-release
 SEMVER="$(php -v | head -n 1 | cut -f 2 -d ' ' | cut -f 1 -d '-')"
