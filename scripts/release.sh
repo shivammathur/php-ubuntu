b6a99ad0295e92735d605cda47095a1506f7e291
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-13 00:29:59 +0530

    Rename src to scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,22 +5,22 @@ add_assets() {
 }
 
 release_cds() {
-  sudo cp ./src/cds /usr/local/bin/cds && sudo sed -i "s|REPO|$GITHUB_REPOSITORY|" /usr/local/bin/cds && sudo chmod a+x /usr/local/bin/cds
+  sudo cp ./scripts/cds /usr/local/bin/cds && sudo sed -i "s|REPO|$GITHUB_REPOSITORY|" /usr/local/bin/cds && sudo chmod a+x /usr/local/bin/cds
   if [[ "$GITHUB_MESSAGE" != *skip-cloudsmith* ]]; then
-    cp ./src/install.sh ./src/php-ubuntu.sh
-    echo "${assets[@]}" ./src/php-ubuntu.sh | xargs -n 1 -P $(("${#assets}"+1)) cds
+    cp ./scripts/install.sh ./scripts/php-ubuntu.sh
+    echo "${assets[@]}" ./scripts/php-ubuntu.sh | xargs -n 1 -P $(("${#assets}"+1)) cds
   fi
 }
 
 release_create() {
   release_cds
-  gh release create "builds" ./src/install.sh "${assets[@]}" -n "builds $version" -t "builds"
+  gh release create "builds" ./scripts/install.sh "${assets[@]}" -n "builds $version" -t "builds"
 }
 
 release_upload() {
   gh release download -p "build.log" || true
   release_cds
-  gh release upload "builds" ./src/install.sh "${assets[@]}" --clobber
+  gh release upload "builds" ./scripts/install.sh "${assets[@]}" --clobber
 }
 
 log() {
