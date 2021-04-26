4b75e95769e539b5b55a9ed7f484ac4367492d45
Shivam Mathur <shivam_jpr@hotmail.com>
2021-04-26 16:09:49 +0530

    Fix package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,12 +2,12 @@ cd / || exit 1
 git add /bin /lib /lib64 /sbin /usr /var
 find /etc -maxdepth 1 -mindepth 1 -type d -exec git add {} \;
 git commit -m "installed php"
-mkdir -p /tmp/php
+mkdir -p /tmp/php/etc/apt/sources.list.d /tmp/php/etc/apt/trusted.gpg.d
 for file in $(git log -p -n 1 --name-only | sed 's/^.*\(\s\).*$/\1/' | xargs -L1 echo); do
   sudo cp -r -p --parents "$file" /tmp/php || true
 done
-sudo cp /etc/apt/sources.list.d/ondrej* /tmp/php/etc/apt/sources.list.d
-sudo cp /etc/apt/trusted.gpg.d/ondrej* /tmp/php/etc/apt/trusted.gpg.d
+sudo cp /etc/apt/sources.list.d/ondrej* /tmp/php/etc/apt/sources.list.d/
+sudo cp /etc/apt/trusted.gpg.d/ondrej* /tmp/php/etc/apt/trusted.gpg.d/
 sudo rm -rf /tmp/php/var/lib/dpkg/alternatives/*
 . /etc/lsb-release
 SEMVER="$(php -v | head -n 1 | cut -f 2 -d ' ' | cut -f 1 -d '-')"
