7805db6d74b106927bbcacf801b465fe1f46f18c
Shivam Mathur <shivam_jpr@hotmail.com>
2021-09-04 06:44:31 +0530

    Release to cds

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,18 +8,18 @@ release_cds() {
   sudo cp ./scripts/cds /usr/local/bin/cds && sudo sed -i "s|REPO|$GITHUB_REPOSITORY|" /usr/local/bin/cds && sudo chmod a+x /usr/local/bin/cds
   if [[ "$GITHUB_MESSAGE" != *skip-cloudsmith* ]]; then
     cp ./scripts/install.sh ./scripts/php-ubuntu.sh
-    echo "${assets[@]}" ./scripts/php-ubuntu.sh | xargs -n 1 -P $(("${#assets}"+1)) cds
+    echo "${assets[@]}" ./scripts/php-ubuntu.sh | xargs -n 1 -P 8 cds
   fi
 }
 
 release_create() {
-  #release_cds
+  release_cds
   gh release create "builds" ./scripts/install.sh "${assets[@]}" -n "builds $version" -t "builds"
 }
 
 release_upload() {
   gh release download -p "build.log" || true
-  #release_cds
+  release_cds
   gh release upload "builds" ./scripts/install.sh "${assets[@]}" --clobber
 }
 
