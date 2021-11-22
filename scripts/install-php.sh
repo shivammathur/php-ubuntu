a8862bb644101a2ebeba19b2d7e135c1c756d543
Shivam Mathur <shivam_jpr@hotmail.com>
2021-11-22 14:34:11 +0000

    Add libpcre2 to the cache

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -5,7 +5,7 @@ sed -i "s/^php_versions.*/php_versions=$PHP_VERSION/" /tmp/php.sh
 sed -i '/ snmp\|php-pear\|composer\|phpunit\|invoke_tests\|source/d' /tmp/php.sh
 sudo DEBIAN_FRONTEND=noninteractive bash /tmp/php.sh || true
 sudo rm -rf /var/cache/apt/archives/*.deb || true
-sudo apt-get install libpcre3-dev libpq-dev unixodbc-dev -y || true
+sudo apt-get install libpcre3-dev libpcre2-dev libpcre2-8-0 libpq-dev unixodbc-dev -y || true
 for extension in ast pcov; do
   sudo apt-get install "php$PHP_VERSION-$extension" -y 2>/dev/null || true
 done
