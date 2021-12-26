064e79bd62298898dc87b3cbdc1a06bb9313f230
Shivam Mathur <shivam_jpr@hotmail.com>
2021-12-26 12:06:30 +0000

    Revert to use packages for PCOV

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -62,7 +62,6 @@ fi
 
 if [[ $PHP_VERSION != "5.6" ]]; then
   apt-fast install -y --no-install-recommends php$PHP_VERSION-ds
-  phpdismod -v $PHP_VERSION pcov
 fi
 
 if [[ $PHP_VERSION = "7.0" || $PHP_VERSION = "7.1" ]]; then
@@ -71,15 +70,9 @@ fi
 
 apt-fast install -y --no-install-recommends php-pear
 
-for extension in ast; do
+for extension in ast pcov; do
   sudo apt-get install "php$PHP_VERSION-$extension" -y 2>/dev/null || true
 done
-
-if [[ $PHP_VERSION =~ 7.[1-4]|8.[0-1] ]]; then
-  sudo pecl install pcov
-  sudo curl -o /etc/php/"$PHP_VERSION"/mods-available/pcov.ini -sL https://raw.githubusercontent.com/shivammathur/php-builder/main/config/modules/pcov.ini
-  phpenmod -v "$PHP_VERSION" pcov
-fi
 
 if [[ $PHP_VERSION =~ 7.[3-4]|8.[0-1] ]]; then
   for extension in sqlsrv pdo_sqlsrv; do
