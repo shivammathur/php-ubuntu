eaf9087bb0dd88cdc846581fe80c8f5943ac5129
Shivam Mathur <shivam_jpr@hotmail.com>
2023-12-09 06:57:05 +0000

    Fix caching PHP 8.4 --skip-cache

--- scripts/extensions.sh
+++ scripts/extensions.sh
@@ -12,17 +12,18 @@ enable_pecl_extension() {
 }
 
 configure_swoole() {
-  fetch_module swoole
-  pecl_ini_file="$(sudo pecl config-get php_ini)"
+  pecl_ini_file=$1
+  fetch_module swoole  
   if [[ -n "$pecl_ini_file" && -e "$pecl_ini_file" ]]; then
     grep -q swoole "$pecl_ini_file" && sudo sed -i -e '/swoole/d' "$pecl_ini_file"
   fi
 }
 
 add_swoole() {
+  pecl_ini_file="$(sudo pecl config-get php_ini)"
   if [[ "$PHP_VERSION" =~ 7.[2-4] ]]; then
-    sudo pecl install -f swoole-4.8.13 && configure_swoole
+    sudo pecl install -f swoole-4.8.13 && configure_swoole "$pecl_ini_file"
   elif [[ "$PHP_VERSION" =~ 8.[0-4] ]]; then
-    sudo pecl install -f swoole && configure_swoole
+    sudo pecl install -f swoole && configure_swoole "$pecl_ini_file"
   fi
 }
