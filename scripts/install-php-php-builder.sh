1e3f380bf8656893ad98f507c5662701ae002b58
Shivam Mathur <shivam_jpr@hotmail.com>
2024-05-16 11:43:00 +0000

    Test

--- scripts/install-php-php-builder.sh
+++ scripts/install-php-php-builder.sh
@@ -4,3 +4,9 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y libxpm-dev libwebp-dev libpcre
 curl -o /tmp/install.sh -sL "https://github.com/shivammathur/php-builder/releases/download/$PHP_VERSION/install.sh"
 bash /tmp/install.sh github "$PHP_VERSION" "${BUILDS:?}" "${TS:?}"
 add_swoole
+
+. /etc/os-release
+. ./scripts/packages.sh
+if [ "$VERSION_ID" = '24.04' ]; then
+  purge_packages libdav1d-dev libgd-dev libxpm-dev libheif-dev
+fi  
