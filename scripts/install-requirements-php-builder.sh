4671a40d0110765146af53426bb5883d341622e1
Shivam Mathur <shivam_jpr@hotmail.com>
2024-07-11 06:45:35 +0000

    Add libraqm-dev and libimagequant-dev

--- scripts/install-requirements-php-builder.sh
+++ scripts/install-requirements-php-builder.sh
@@ -6,9 +6,9 @@ add_ppa
 
 libenchant_dev=$(apt-cache show libenchant-?[0-9]+?-dev | grep 'Package' | head -n 1 | cut -d ' ' -f 2)
 DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 apt-fast automake gcc g++ git jq make pkg-config shtool libtool sudo systemd unzip
-DEBIAN_FRONTEND=noninteractive apt-get install -y autoconf firebird-dev freetds-dev libacl1-dev libapparmor-dev libargon2-dev libaspell-dev libc-client2007e-dev libcurl4-openssl-dev libdb-dev libedit-dev "$libenchant_dev" libfreetype6-dev libgomp1 libicu-dev libjpeg-dev libkrb5-dev libldap-dev liblmdb-dev liblz4-dev libmagickwand-dev libmemcached-dev libonig-dev libpng-dev libpq-dev libqdbm-dev librabbitmq-dev libsodium-dev libsnmp-dev libsqlite3-dev libtidy-dev libtool libwrap0-dev libxml2-dev libxslt1-dev libyaml-dev libzip-dev libzmq3-dev libzstd-dev make php-common snmp shtool systemd tzdata
+DEBIAN_FRONTEND=noninteractive apt-get install -y autoconf firebird-dev freetds-dev libacl1-dev libapparmor-dev libargon2-dev libaspell-dev libc-client2007e-dev libcurl4-openssl-dev libdb-dev libedit-dev "$libenchant_dev" libfreetype6-dev libraqm-dev libimagequant-dev libgomp1 libicu-dev libjpeg-dev libkrb5-dev libldap-dev liblmdb-dev liblz4-dev libmagickwand-dev libmemcached-dev libonig-dev libpng-dev libpq-dev libqdbm-dev librabbitmq-dev libsodium-dev libsnmp-dev libsqlite3-dev libtidy-dev libtool libwrap0-dev libxml2-dev libxslt1-dev libyaml-dev libzip-dev libzmq3-dev libzstd-dev make php-common snmp shtool systemd tzdata
 purge_packages libfile-fcntllock-perl libalgorithm-merge-perl libalgorithm-diff-xs-perl unattended-upgrades libalgorithm-diff-perl manpages-dev
-purge_packages libgd-dev libgd3 libavif13 libyuv0 libaom3 libdav1d5 libgav1-0 libabsl20210324 || true
+purge_packages libgd-dev libgd3 libavif13 libavif16 libimagequant0 libraqm0 libyuv0 libaom3 libdav1d5 libgav1-0 libabsl20210324 libdav1d7 libgav1-1 librav1e0 libsvtav1enc1d1 libabsl20220623t64 || true
 if [ "$VERSION_ID" != '24.04' ]; then
   DEBIAN_FRONTEND=noninteractive apt-get install -y unixodbc
 fi
