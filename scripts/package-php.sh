e2edad428037edb616d1caee346733e6c4b1a6c1
Shivam Mathur <shivam_jpr@hotmail.com>
2025-04-13 12:20:55 +0000

    Fix PPA source file [skip ci]

--- scripts/package-php.sh
+++ scripts/package-php.sh
@@ -6,7 +6,7 @@ for dir_path in /bin /lib /lib64 /sbin /usr /var /run/php; do
 done
 find /etc -maxdepth 1 -mindepth 1 -type d -exec git add {} \;
 git commit -m "installed php"
-mkdir -p /tmp/php/etc/apt/sources.list.d /tmp/php/etc/apt/trusted.gpg.d /tmp/php/var/lib/apt/lists
+mkdir -p /tmp/php/etc/apt/sources.list.d /tmp/php/etc/apt/trusted.gpg.d /tmp/php/var/lib/apt/lists /tmp/php/usr/share/keyrings
 for file in $(git log -p -n 1 --name-only | sed 's/^.*\(\s\).*$/\1/' | xargs -L1 echo); do
   if [ -e "$file" ]; then
     sudo cp -r -p --parents "$file" /tmp/php || true
@@ -25,6 +25,7 @@ sudo cp /etc/apt/trusted.gpg.d/ondrej* /tmp/php/etc/apt/trusted.gpg.d/
 sudo cp /var/lib/apt/lists/*ondrej* /tmp/php/var/lib/apt/lists/
 sudo cp -a /usr/lib/"$lib_subdir"/libpcre* /tmp/php/usr/lib/"$lib_subdir"/
 sudo cp -a /lib/"$lib_subdir"/libpcre* /tmp/php/usr/lib/"$lib_subdir"/
+sudo cp -a /usr/share/keyrings/ondrej-php-keyring.gpg /tmp/php/usr/share/keyrings/ondrej-php-keyring.gpg
 sudo rm -rf /tmp/php/var/lib/dpkg/alternatives/* /tmp/php/var/lib/dpkg/status-old /tmp/php/var/lib/dpkg/status-orig
 . /etc/os-release
 SEMVER="$(php -v | head -n 1 | cut -f 2 -d ' ' | cut -f 1 -d '-')"
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -14,17 +14,62 @@ purge_packages() {
     done
 }
 
-add_ppa() {
-    . /etc/os-release
-    export _APTMGR=apt-get
-    apt-get update && apt-get install -y curl sudo software-properties-common
-    add-apt-repository ppa:git-core/ppa
-    add-apt-repository ppa:apt-fast/stable
-    LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
-    [ "${BUILDS:?}" = "debug" ] && sed -i "h;s/^//;p;x" /etc/apt/sources.list.d/ondrej-*.list && sed -i '2s/main$/main\/debug/' /etc/apt/sources.list.d/ondrej-*.list
-    curl -sL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-    curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-    echo "deb https://apt.postgresql.org/pub/repos/apt/ $VERSION_CODENAME-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
-    echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/$VERSION_ID/prod $VERSION_CODENAME main" | tee /etc/apt/sources.list.d/microsoft-prod.list
-    apt-get update
+get() {
+  mode=$1
+  file_path=$2
+  shift 2
+  links=("$@")
+  if [ "$mode" = "-s" ]; then
+    sudo curl -sL "${links[0]}"
+  else
+    for link in "${links[@]}"; do
+      status_code=$(sudo curl -w "%{http_code}" -o "$file_path" -sL "$link")
+      [ "$status_code" = "200" ] && break
+    done
+  fi
 }
+
+get_launchpad_key() {
+  ppa=$1
+  branches=$2
+  sks=(
+    'https://keyserver.ubuntu.com'
+    'https://pgp.mit.edu'
+    'https://keys.openpgp.org'
+  )
+  lp_api=(
+    'https://api.launchpad.net/1.0'
+    'https://api.launchpad.net/devel'
+  )
+  key_file=/usr/share/keyrings/"${ppa/\//-}"-keyring.gpg
+  fingerprint="$(get -s "" "${lp_api[@]/%//~${ppa%/*}/+archive/${ppa##*/}}" | jq -r '.signing_key_fingerprint')"
+  sks_params="op=get&options=mr&exact=on&search=0x$fingerprint"
+  key_urls=("${sks[@]/%/\/pks\/lookup\?"$sks_params"}")
+  get -q "$key_file" "${key_urls[@]}"
+  if [[ "$(file "$key_file")" =~ .*('Public-Key (old)'|'Secret-Key') ]]; then
+    sudo gpg --batch --yes --dearmor "$key_file"  && sudo mv "$key_file".gpg "$key_file"
+  fi
+}
+
+add_ondrej_ppa() {
+  get_launchpad_key ondrej/php main
+  ondrej_ppa_source="deb [arch=$arch signed-by=/usr/share/keyrings/ondrej-php-keyring.gpg] https://ppa.launchpadcontent.net/ondrej/php/ubuntu $VERSION_CODENAME main"
+  echo "$ondrej_ppa_source" | tee /etc/apt/sources.list.d/ondrej-php.list
+  [ "${BUILDS:?}" = "debug" ] && echo "$ondrej_ppa_source/debug" | tee -a /etc/apt/sources.list.d/ondrej-php.list
+}
+
+add_ppa() {
+  . /etc/os-release
+  export _APTMGR=apt-get
+  apt-get update && apt-get install -y curl sudo file jq software-properties-common
+  add-apt-repository ppa:git-core/ppa
+  add-apt-repository ppa:apt-fast/stable
+  add_ondrej_ppa
+  curl -sL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+  curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+  echo "deb https://apt.postgresql.org/pub/repos/apt/ $VERSION_CODENAME-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+  echo "deb [arch=$arch] https://packages.microsoft.com/ubuntu/$VERSION_ID/prod $VERSION_CODENAME main" | tee /etc/apt/sources.list.d/microsoft-prod.list
+  apt-get update
+}
+
+arch=$(dpkg --print-architecture)
