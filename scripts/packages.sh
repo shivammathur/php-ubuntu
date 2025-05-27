518fadc6ac8da68f8b11815fdf352c4885503452
Shivam Mathur <shivam_jpr@hotmail.com>
2025-05-27 08:15:39 +0000

    Remove glib packages from the required list [skip ci]

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -15,23 +15,28 @@ purge_packages() {
 }
 
 get() {
-  mode=$1
-  file_path=$2
+  local mode=$1
+  local file_path=$2
   shift 2
   links=("$@")
   if [ "$mode" = "-s" ]; then
-    sudo curl -sL "${links[0]}"
+    curl -sL "${links[0]}"
   else
     for link in "${links[@]}"; do
-      status_code=$(sudo curl -w "%{http_code}" -o "$file_path" -sL "$link")
+      status_code=$(curl -w "%{http_code}" -o "$file_path" -sL "$link")
       [ "$status_code" = "200" ] && break
     done
   fi
 }
 
+ubuntu_fingerprint() {
+  local ppa=$1
+  get -s -n "" "${lp_api[@]/%//~${ppa%/*}/+archive/${ppa##*/}}" | jq -r '.signing_key_fingerprint'
+}
+
 get_launchpad_key() {
-  ppa=$1
-  branches=$2
+  local pa=$1
+  local key_file=$2
   sks=(
     'https://keyserver.ubuntu.com'
     'https://pgp.mit.edu'
@@ -40,31 +45,34 @@ get_launchpad_key() {
   lp_api=(
     'https://api.launchpad.net/1.0'
     'https://api.launchpad.net/devel'
-  )
-  key_file=/usr/share/keyrings/"${ppa/\//-}"-keyring.gpg
+  )  
   fingerprint="$(get -s "" "${lp_api[@]/%//~${ppa%/*}/+archive/${ppa##*/}}" | jq -r '.signing_key_fingerprint')"
   sks_params="op=get&options=mr&exact=on&search=0x$fingerprint"
   key_urls=("${sks[@]/%/\/pks\/lookup\?"$sks_params"}")
   get -q "$key_file" "${key_urls[@]}"
   if [[ "$(file "$key_file")" =~ .*('Public-Key (old)'|'Secret-Key') ]]; then
-    sudo gpg --batch --yes --dearmor "$key_file"  && sudo mv "$key_file".gpg "$key_file"
+    gpg --batch --yes --dearmor "$key_file" && mv "$key_file".gpg "$key_file"
   fi
 }
 
-add_ondrej_ppa() {
-  get_launchpad_key ondrej/php main
-  ondrej_ppa_source="deb [arch=$arch signed-by=/usr/share/keyrings/ondrej-php-keyring.gpg] https://ppa.launchpadcontent.net/ondrej/php/ubuntu $VERSION_CODENAME main"
-  echo "$ondrej_ppa_source" | tee /etc/apt/sources.list.d/ondrej-php.list
-  [ "${BUILDS:?}" = "debug" ] && echo "$ondrej_ppa_source/debug" | tee -a /etc/apt/sources.list.d/ondrej-php.list
+add_ppa_helper() {
+  local ppa=$1
+  local branches=$2
+  ppa_url="https://ppa.launchpadcontent.net/$ppa/ubuntu"
+  key_file=/usr/share/keyrings/"${ppa/\//-}"-keyring.gpg
+  list_dir=/etc/apt/sources.list.d
+  get_launchpad_key "$ppa" "$key_file"
+  . /etc/os-release
+  echo "deb [arch="$(dpkg --print-architecture)" signed-by=$key_file] $ppa_url $VERSION_CODENAME $branches" | tee -a "$list_dir"/"${ppa/\//-}".list
 }
 
 add_ppa() {
   . /etc/os-release
   export _APTMGR=apt-get
-  apt-get update && apt-get install -y curl sudo file jq software-properties-common
-  add-apt-repository ppa:git-core/ppa
-  add-apt-repository ppa:apt-fast/stable
-  add_ondrej_ppa
+  apt-get update && apt-get install -y curl sudo file jq gnupg
+  add_ppa_helper apt-fast/stable main
+  add_ppa_helper git-core/ppa main
+  add_ppa_helper ondrej/php main
   curl -sL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
   curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
   echo "deb https://apt.postgresql.org/pub/repos/apt/ $VERSION_CODENAME-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
