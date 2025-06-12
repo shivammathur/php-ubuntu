cbf2c3f9035c3fa9260d222b8baa84f2a9cee254
Shivam Mathur <shivam_jpr@hotmail.com>
2025-06-12 15:51:40 +0000

    Switch to release artifacts for zstd [skip ci]

--- scripts/install-zstd.sh
+++ scripts/install-zstd.sh
@@ -1,17 +1,16 @@
 mkdir -p /opt/zstd
-zstd_url=$(curl -sL https://api.github.com/repos/"$REPO"/actions/artifacts | jq -r --arg zstd_dir "${ZSTD_DIR:?}-${CONTAINER//[\/:]/-}" '.artifacts[] | select(.name=="\($zstd_dir)").archive_download_url' 2>/dev/null | head -n 1)
-if [ "x$zstd_url" = "x" ] || [ "x$USE_CACHE" = "xfalse" ]; then
+curl -o /tmp/zstd.tar.gz -sL "https://github.com/shivammathur/php-ubuntu/releases/download/builds/${ZSTD_DIR:?}-${CONTAINER//[\/:]/-}.tar.gz"
+tar -xzf /tmp/zstd.tar.gz -C /opt/zstd || true
+if ! [ -e /opt/zstd/bin ]; then
   apt-get install zlib1g-dev liblzma-dev liblz4-dev -y
-  curl -o /tmp/zstd.tar.gz -sL https://github.com/facebook/zstd/releases/latest/download/"$ZSTD_DIR".tar.gz
-  tar -xzf /tmp/zstd.tar.gz -C /tmp
+  curl -o /tmp/zstd-src.tar.gz -sL https://github.com/facebook/zstd/releases/latest/download/"$ZSTD_DIR".tar.gz
+  tar -xzf /tmp/zstd-src.tar.gz -C /tmp
   (
     cd /tmp/"$ZSTD_DIR" || exit 1
     make install -j"$(nproc)" PREFIX=/opt/zstd
   )
+  apt-get purge zlib1g-dev liblzma-dev liblz4-dev -y
 else
-  curl -u "$USER":"$TOKEN" -o /tmp/zstd.zip -sL "$zstd_url"
-  ls /tmp
-  unzip /tmp/zstd.zip -d /opt/zstd
   chmod -R a+x /opt/zstd/bin
 fi
 ln -sf /opt/zstd/bin/* /usr/local/bin
