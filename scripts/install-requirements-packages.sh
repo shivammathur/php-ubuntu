71b54dc365e713a8eb2ccb5910d4c0b732ab1ca5
Shivam Mathur <shivam_jpr@hotmail.com>
2026-01-09 05:56:34 +0000

    Add libsodium-dev in requirements [skip ci]

--- scripts/install-requirements-packages.sh
+++ scripts/install-requirements-packages.sh
@@ -7,5 +7,5 @@ add_packages apache2 apt-fast automake file gcc g++ git jq make pkg-config shtoo
 purge_packages libfile-fcntllock-perl libalgorithm-merge-perl libalgorithm-diff-xs-perl unattended-upgrades libalgorithm-diff-perl manpages-dev
 add_packages php"${PHP_VERSION:?}"-common php"${PHP_VERSION:?}"-imagick
 purge_packages php"${PHP_VERSION:?}"-imagick php"${PHP_VERSION:?}"-common php-common
-add_packages snmp snmp-mibs-downloader firebird-dev freetds-dev libargon2-dev libaspell-dev libc-client2007e libcurl4-openssl-dev libdb-dev libexpat1-dev libhunspell-dev libjson-c-dev libkmod-dev libnorm-dev libpgm-dev libpq-dev libqdbm-dev librabbitmq-dev libsnmp-dev libssl-dev libtidy-dev libtiff-dev libtommath-dev libwebp-dev libxpm-dev libxslt1-dev libyaml-dev libzip-dev udev tzdata
+add_packages snmp snmp-mibs-downloader firebird-dev freetds-dev libargon2-dev libaspell-dev libc-client2007e libcurl4-openssl-dev libdb-dev libexpat1-dev libhunspell-dev libjson-c-dev libkmod-dev libnorm-dev libpgm-dev libpq-dev libqdbm-dev librabbitmq-dev libsodium-dev libsnmp-dev libssl-dev libtidy-dev libtiff-dev libtommath-dev libwebp-dev libxpm-dev libxslt1-dev libyaml-dev libzip-dev udev tzdata
 purge_packages libgd3 libimagequant0 libraqm0 libavif13 libyuv0 libaom3 libdav1d5 libgav1-0 libabsl20210324 debsuryorg-archive-keyring
