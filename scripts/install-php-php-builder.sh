077d2f8404da4a44731bde1513eb3f616a83fe0f
Shivam Mathur <shivam_jpr@hotmail.com>
2026-05-08 08:29:13 +0530

    Install libpng-dev with PHP cache builds [skip-cache] [skip-nightly-cache]

--- scripts/install-php-php-builder.sh
+++ scripts/install-php-php-builder.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 cp /var/lib/dpkg/status /var/lib/dpkg/status-orig
-DEBIAN_FRONTEND=noninteractive apt-get install -y libxpm-dev libwebp-dev libpcre3-dev libpcre2-dev libxmlrpc-epi-dev libblkid-dev libmount-dev libselinux1-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y libxpm-dev libwebp-dev libpcre3-dev libpcre2-dev libpng-dev libxmlrpc-epi-dev libblkid-dev libmount-dev libselinux1-dev
 curl -o /tmp/install.sh -sL "https://github.com/shivammathur/php-builder/releases/download/$PHP_VERSION/install.sh"
 bash /tmp/install.sh github "$PHP_VERSION" "${BUILDS:?}" "${TS:?}"
 
 . /etc/os-release
 . ./scripts/packages.sh
 if [ "$VERSION_ID" = '24.04' ]; then
-  purge_packages libbz2-dev libcairo2-dev libdav1d-dev libfontconfig-dev libfreetype-dev libgd-dev libgdk-pixbuf-2.0-dev libheif-dev libmagickcore-dev libmagickcore-6.q16-dev libpng-dev libwmf-dev libxpm-dev
+  purge_packages libbz2-dev libcairo2-dev libdav1d-dev libfontconfig-dev libfreetype-dev libgd-dev libgdk-pixbuf-2.0-dev libheif-dev libmagickcore-dev libmagickcore-6.q16-dev libwmf-dev libxpm-dev
   add_packages libheif-plugin-libde265
 fi
 dev_only_packages=(
